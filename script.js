cd2d4a9aefe6dd41f4cfc9eadbdf34e90d638d84
Joao Marcelo <jm.castro@usp.br>
2022-04-08 23:11:38 -0300

    distribuição de cartas aleatorias funcionando

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
+const cartas = [];
 const gifs = [
-    "bobrossparrot",
-    "explodyparrot",
-    "fiestaparrot",
-    "metalparrot",
-    "revertitparrot",
-    "tripletsparrot",
-    "unicornparrot"
+    {image:"bobrossparrot"},
+    {image:"explodyparrot"},
+    {image:"fiestaparrot"},
+    {image:"metalparrot"},
+    {image:"revertitparrot"},
+    {image:"tripletsparrot"},
+    {image:"unicornparrot"}
 ];
 
 function checkCartas(){
     let qtdCartas = Number(prompt("Com quantas cartas deseja jogar?: "));
     if (qtdCartas % 2 === 0 && qtdCartas >= 4 && qtdCartas <= 14){
-         let i = 0;
+        inserirGifs(qtdCartas);
+        let i = 0;
          while(i < qtdCartas){
             inserirCartas(i);
             i++;
@@ -22,6 +24,22 @@ function checkCartas(){
     }
 }
 
+function comparador(){
+    return Math.random() - 0.5;
+}
+
+function inserirGifs(qtdCartas) {
+    for (let i = 0; i < qtdCartas / 2; i++) {
+        let aux = 0;
+        
+        while (aux < 2) {
+            cartas.push(gifs[i].image);
+            aux++;
+        }
+    }
+    cartas.sort(comparador);
+}
+
 function inserirCartas(i){
     let elemento = document.querySelector(".container");
     elemento.innerHTML += 
@@ -30,23 +48,18 @@ function inserirCartas(i){
             <img src="/images/front.png" alt="">
         </div>
         <div class="frente face">
-            <img src="/images/${gifs[i]}.gif" alt="">
+            <img src="/images/${cartas[i]}.gif" alt="">
         </div>
     </div>`;
 }
 
 function viraCartas(){
-
-}
-
-function comparador(){
-    return Math.random() - 0.5;
-}
-
-function misturaCartas(){
-    const listaCarta = document.querySelectorAll(".carta")
-    listaCarta.slice.call().sort(comparador)
+    //viro a primeira carta
+    //if (segunda carta %2 === 0){ as duas ficam viradas para cima }
+    //else {o usuario errou. aguarde 1 segundo. vire para baixo as duas cartas clicadas}
+    
+    
+    //comparara objetos para saber se o usuario
 }
 
 checkCartas();
-// misturaCartas();
