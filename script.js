0fc17481406761dfa2712d2c0108d7639917b34d
Joao Marcelo <jm.castro@usp.br>
2022-04-12 15:10:16 -0300

    refatoração do código e correção de bugs

--- script.js
+++ script.js
@@ -1,126 +1,128 @@
-let qtdCartas = Number(prompt("Com quantas cartas deseja jogar?"));
-const cartas = [];
-const gifs = [
-    {image:"bobrossparrot"},
-    {image:"explodyparrot"},
-    {image:"fiestaparrot"},
-    {image:"metalparrot"},
-    {image:"revertitparrot"},
-    {image:"tripletsparrot"},
-    {image:"unicornparrot"}
+let qtdCartas;
+let tipos = [
+  "bobrossparrot",
+  "explodyparrot",
+  "fiestaparrot",
+  "metalparrot",
+  "revertitparrot",
+  "tripletsparrot",
+  "unicornparrot"
 ];
+let baralho = [];
 
-function checkCartas(){
-    if (qtdCartas % 2 === 0 && qtdCartas >= 4 && qtdCartas <= 14){
-        inserirGifs();
-        let i = 0;
-         while(i < qtdCartas){
-            inserirCartas(i);
-            i++;
-        }    
-    }else{
-        alert("Insira um número par entre 4 e 14 para jogar");
-        qtdCartas = Number(prompt("Com quantas cartas deseja jogar?"));
-        checkCartas();
+let primeiraCarta;
+let segundaCarta;
+let jogadas = 0;
+let cartasCertas = 0;
+let timer = 0;
+let idTimer;
+
+function perguntarQuantidade() {
+  qtdCartas = parseInt(prompt("Com quantas cartas você quer jogar?"));
+  //qtdCartas = 4;
+  while (
+    isNaN(qtdCartas) ||
+    qtdCartas < 4 ||
+    qtdCartas > 14 ||
+    qtdCartas % 2 === 1
+  ) {
+    qtdCartas = parseInt(prompt("Com quantas cartas você quer jogar?"));
+  }
+}
+
+function gerarBaralho() {
+  for (let i = 0; i < qtdCartas / 2; i++) {
+    const carta = tipos[i];
+    baralho.push(carta);
+    baralho.push(carta);
+  }
+  baralho.sort(comparador);
+  renderizarCartas();
+}
+
+function renderizarCartas() {
+  const tabuleiro = document.querySelector(".container");
+
+  for (let i = 0; i < baralho.length; i++) {
+    let templateCarta = `
+            <div class="carta" onClick="virarCarta(this)">
+                <div class='front-face face'>
+                    <img src='images/front.png'>
+                </div>
+                <div class='back-face face'>
+                    <img src='images/${baralho[i]}.gif'>
+                </div>
+            </div>
+        `;
+    tabuleiro.innerHTML += templateCarta;
+  }
+}
+
+function virarCarta(cartaClicada) {
+  if (primeiraCarta === undefined && jogadas === 0) {
+    inicarTimer();
+  }
+  if (cartaClicada.classList.contains("virada") || segundaCarta !== undefined) {
+    return;
+  }
+
+  jogadas++;
+  cartaClicada.classList.add("virada");
+
+  if (primeiraCarta === undefined) {
+    primeiraCarta = cartaClicada;
+  } else {
+    segundaCarta = cartaClicada;
+    //SE TIVER CLICADO EM DUAS
+    if (primeiraCarta.innerHTML === segundaCarta.innerHTML) {
+      cartasCertas += 2;
+      //VERIFICAR SE O JOGO TERMINOU
+      verificarFimJogo();
+      resetarCartas();
+    } else {
+      //CLICOU EM DUAS E NÃO ACERTOU
+      setTimeout(desvirarCartas, 1000);
     }
+  }
+}
+//SO SABE DESVIRAR CARTAS
+function desvirarCartas() {
+  primeiraCarta.classList.remove("virada");
+  segundaCarta.classList.remove("virada");
+  resetarCartas();
+}
+//SO SABE ZERAR VARIAVEIS
+function resetarCartas() {
+  primeiraCarta = undefined;
+  segundaCarta = undefined;
 }
 
-function comparador(){
-    return Math.random() - 0.5;
-}
+function verificarFimJogo() {
+  if (cartasCertas === qtdCartas) {
+    clearInterval(idTimer);
+    setTimeout(
+      alert,
+      1000,
+      `Você venceu em ${jogadas} jogadas e em ${timer} segundos`
+    );
 
-function inserirGifs() {
-    for (let i = 0; i < qtdCartas / 2; i++) {
-        let aux = 0;
-        
-        while (aux < 2) {
-            cartas.push(gifs[i].image);
-            aux++;
-        }
+    const resposta = prompt("Você quer jogar novamente?");
+    if (resposta === "sim") {
+      window.location.reload();
     }
-    cartas.sort(comparador);
+  }
 }
 
-function inserirCartas(i){
-    let elemento = document.querySelector(".container");
-    elemento.innerHTML += 
-    `<div class="carta" onclick="viraCartas(this)">
-        <div class="face">
-            <img src="/images/front.png" alt="">
-        </div>
-        <div class="back-face face">
-            <img src="/images/${cartas[i]}.gif" alt="">
-        </div>
-    </div>`;
+function comparador() {
+  return Math.random() - 0.5;
 }
 
-let contadorTempo = 0;
-let numJogadas = 0;
-let acertos = 0;
-let intervalo = 0;
+perguntarQuantidade();
+gerarBaralho();
 
-function viraCartas(elemento){
-    let selecionadoCartaVerso = document.querySelector(".selecionado-verso");
-    let selecionadoCartaFrente = document.querySelector(".selecionado-frente")
-
-    elemento.querySelector(".face").classList.add("selecionado-frente");
-    elemento.querySelector(".back-face").classList.add("selecionado-verso")
-
-    if(selecionadoCartaVerso !== null) {
-        if (elemento.querySelector(".back-face").innerHTML === selecionadoCartaVerso.innerHTML){
-            elemento.querySelector(".face").classList.add("match-frente");
-            elemento.querySelector(".back-face").classList.add("match-verso");
-            selecionadoCartaFrente.classList.add("match-frente");
-            selecionadoCartaVerso.classList.add("match-verso");
-            acertos++;
-        }
-        setTimeout(function() {
-            elemento.querySelector(".face").classList.remove("selecionado-frente");
-            elemento.querySelector(".back-face").classList.remove("selecionado-verso");
-            document.querySelector(".selecionado-verso").classList.remove("selecionado-verso");
-            document.querySelector(".selecionado-frente").classList.remove("selecionado-frente");
-        }, 1000);
-    }
-    numJogadas++;
-    checkGanhou();
+function inicarTimer() {
+  idTimer = setInterval(function () {
+    timer++;
+    document.querySelector(".timer").innerHTML = timer;
+  }, 1000);
 }
-
-
-function checkGanhou(){
-    if(acertos === qtdCartas/2) {
-        setTimeout( function(){
-            alert(`Você ganhou em ${numJogadas} jogadas e levou ${contadorTempo} segundos!`);
-        }, 50);
-        setTimeout(function (){
-            jogarDeNovo();
-        }, 2000);
-    }
-}
-
-function timer() {
-    intervalo = setInterval(acrescentaTempo, 1000);
-}
-  
-function acrescentaTempo() {
-    contadorTempo++;
-    document.querySelector(".timer").innerHTML = contadorTempo + "s";
-}
-  
-
-function jogarDeNovo() {
-    let jogarDeNovo = prompt("Quer jogar mais uma partida? (digite 'sim' ou 'não')");
-    jogarDeNovo = jogarDeNovo.toLowerCase();
-
-    if (jogarDeNovo === "sim"){    
-        document.location.reload(true);
-    }else if (jogarDeNovo === "não"){
-        clearInterval(intervalo);
-        alert("Obrigado por jogar!");
-    }else{
-        alert("Resposta inválida!");
-        jogarDeNovo();
-    }
-}
-
-checkCartas();
-timer();
